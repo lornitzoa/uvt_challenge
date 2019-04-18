24b78932f05fcb00c249cdd310ef4261189ab0fd
Anna Lornitzo <lornitzoa@gmail.com>
2019-04-18 16:13:54 -0400

    initial time difference check functional

--- app.js
+++ app.js
@@ -1,23 +1,77 @@
 const app = angular.module('UVTApp', [])
 
 app.controller('MainController', function() {
-  // Variables
-  this.uvt = 0
+  ////////////////////////////
+  //       Variables
+  ////////////////////////////
+  this.uvt = 0 // Unique View Time
   this.duration = 0
   this.durationHH = '00'
   this.durationMM = '00'
   this.durationSS = '00'
-  this.startHH = '00'
-  this.startMM = '00'
-  this.startSS = '00'
-  this.endHH = '00'
-  this.endMM = '00'
-  this.endSS = '00'
+  this.startTime = '00'
+  this.startHH = '01'
+  this.startMM = '45'
+  this.startSS = '05'
+  this.endTime = '00'
+  this.endHH = '05'
+  this.endMM = '03'
+  this.endSS = '30'
+  this.vf = 0
 
-  // Functions
+  ////////////////////////////
+  //       Functions
+  ////////////////////////////
+
+  // set video duration for timeline and other calculations
   this.submitDuration = () => {
-    this.duration = parseInt(this.durationHH) + ':' + parseInt(this.durationMM) + ':' + parseInt(this.durationSS)
+    this.duration = this.durationHH + ':' + this.durationMM + ':' + this.durationSS
+    console.log(new Date('2018-04-15T' + this.duration + 'z'))
   }
+
+  // set view fragment start and end times
+  this.submitVF = () => {
+    console.log('submittingVF');
+    this.startTime = this.startHH + ':' + this.startMM + ':' + this.startSS
+    this.endTime = this.endHH + ':' + this.endMM + ':' + this.endSS
+    let startTime = new Date('2018-04-15T' + this.startTime)
+    let endTime = new Date('2018-04-15T' + this.endTime)
+    this.checkTimeDiff(endTime, startTime)
+
+
+    // let viewFragment = (endTime.getHours() - startTime.getHours()) + ':' + (endTime.getMinutes() - startTime.getMinutes()) + ":" + (endTime.getSeconds() - startTime.getSeconds())
+
+
+
+    // console.log(viewFragment)
+    // console.log(viewFragment.getHours())
+  }
+
+  this.checkTimeDiff = (end, start) => {
+    let hoursDiff = end.getHours() - start.getHours()
+    let minutesDiff = end.getMinutes() - start.getMinutes()
+    let secondsDiff = end.getSeconds() - start.getSeconds()
+    if(minutesDiff < 0) {
+      minutesDiff = 60 + minutesDiff
+      hoursDiff -= 1
+    }
+    if(secondsDiff < 0) {
+      console.log(secondsDiff);
+      secondsDiff = 60 + secondsDiff
+      minutesDiff -= 1
+      if(minutesDiff < 0) {
+        minutesDiff = 60 + minutesDiff
+        hoursDiff -= 1
+      }
+    }
+
+    console.log(hoursDiff + ":" + minutesDiff + ':' + secondsDiff);
+  }
+
+  // create fragment pair and push to array for timeline
+  // check if fragment spans previously watched fragments
+  // if not perfect match, but overlap exists, add additional UVT time to total uvt
+
 
 
 
