220a619d4aa2ee8f9ddca961474d0b75ff0cde64
Anna Lornitzo <lornitzoa@gmail.com>
2019-04-22 12:24:44 -0400

    checkOverlap checks if current location extends into existing UVT and combines overlapped UVTs

--- jquery.js
+++ jquery.js
@@ -30,8 +30,9 @@ $( () => {
   const timer = () => {
     // console.log(currentLocation);
     // increment milliseconds up to 999
+
     if (ms < 995) {
-      ms+= 4 // millisecond incrementation per interval >> after testing, 4 seemed to be the best increment for having the timer close to actual duration of one second.
+      ms += 4 // millisecond incrementation per interval >> after testing, 4 seemed to be the best increment for having the timer close to actual duration of one second.
     } else {
       // reset milliseconds to 0 after reaching 99
       ms = 0
@@ -46,16 +47,28 @@ $( () => {
       }
     }
     // set current location to prepare for stopTime logging
-    currentLocation = (formatTime(m.toString(), s.toString(), ms.toString(), 'number'))
+    currentLocation = (formatTime(m, s, ms, 'number'))
+    // console.log(currentLocation);
     // user current location to set range value/position
     $('#range')[0].value = currentLocation
     // set timeElapsed element; send time increments to formatTime for formatting
-    $('#timeElapsed').text(formatTime(m.toString(), s.toString(), ms.toString(), 'string'))
+    $('#timeElapsed').text(formatTime(m, s, ms, 'string'))
     // create endTime key value for real-time update
     vf[vfIndex].endTime = currentLocation
-    // console.log(vf);
+    // console.log(vf[vfIndex].endTime);
     calcUVT(vf[vfIndex])
 
+  }
+
+  const checkForString = () => {
+    let unitArr = [m, s, ms]
+    for (let i = 0; i < unitArr.length; i++) {
+      if(i.length < 0) {
+        i = 0
+      } else {
+        i = parseInt(i)
+      }
+    }
   }
 
   const calcUVT = (obj) => {
@@ -72,18 +85,18 @@ $( () => {
   }
 
   const checkOverlap = (obj) => {
-
+    console.log(obj.endTime);
     for(let i = 0; i < uvtArr.length; i++) {
-      if(obj.startTime === uvtArr[i].startTime) {
+      if(obj.startTime === uvtArr[i].startTime) { // check if same start time
         // console.log('same start time');
         if(obj.endTime > uvtArr[i].endTime) {
-          console.log('later end time of same start time');
+          // console.log('later end time of same start time');
           // check if end time is in range of any following uvtArr elements
-          console.log(i);
+          // console.log(i);
           for(let x = (i+1); x < uvtArr.length; x++) {
             if(uvtArr[i].endTime >= uvtArr[x].startTime && uvtArr[i].endTime < uvtArr[x].endTime) {
-              console.log('within existing range');
-              console.log(x);
+              // console.log('within existing range');
+              // console.log(x);
               uvtArr[i].endTime = uvtArr[x].endTime
               uvtArr.splice((i+1), x)
             }
@@ -92,19 +105,47 @@ $( () => {
           return true
         }
         return true
-      } else if (obj.startTime > uvtArr[i].startTime && obj.startTime <= uvtArr[i].endTime) {
-        // console.log('start time within range');
+      } else if (obj.startTime > uvtArr[i].startTime && obj.startTime < uvtArr[i].endTime) { // check if start time is within range of existing UVT pair
+        // console.log(obj.endTime);
+        // console.log('start time within range: ' + i );
         if(obj.endTime > uvtArr[i].endTime) {
           // console.log('later end time of same start time');
+          // check if end time is in range of any following uvtArr elements
+          // console.log(i);
+          for(let x = (i+1); x < uvtArr.length; x++) {
+            // console.log(uvtArr[x]);
+            if(obj.endTime >= uvtArr[x].startTime && obj.endTime <= uvtArr[x].endTime) {
+              // console.log('end time within existing range');
+              // console.log(x);
+              uvtArr[i].endTime = uvtArr[x].endTime
+              uvtArr.splice((i+1), x)
+              return true
+            }
+            // if(uvtArr[x].endTime >= uvtArr[i].startTime && uvtArr[x].endTime < uvtArr[i].endTime) {
+            //   console.log('end time within existing range');
+            //   console.log(i);
+            //   // uvtArr[x].endTime = uvtArr[i].endTime
+            //   // uvtArr.splice((i+1), x)
+            // }
+          }
           uvtArr[i].endTime = obj.endTime
           return true
         }
         return true
-      } else if( obj.endTime >= uvtArr[i].startTime && obj.endTime <= uvtArr[i].endTime) {
-        // console.log('end time within range');
-        uvtArr[i].startTime = obj.startTime
-        return true
       }
+      // else if (obj.startTime > uvtArr[i].startTime && obj.startTime <= uvtArr[i].endTime) {
+      //   // console.log('start time within range');
+      //   if(obj.endTime > uvtArr[i].endTime) {
+      //     // console.log('later end time of same start time');
+      //     uvtArr[i].endTime = obj.endTime
+      //     return true
+      //   }
+      //   return true
+      // } else if( obj.endTime >= uvtArr[i].startTime && obj.endTime <= uvtArr[i].endTime) {
+      //   // console.log('end time within range');
+      //   uvtArr[i].startTime = obj.startTime
+      //   return true
+      // }
     }
     return false
   }
@@ -219,9 +260,9 @@ $( () => {
 
   // updated timeElapsed element when slider is moved
   updateTimerVal = (val) => {
-    m = val.slice(-7, -5) // slice minutes from value
-    s = val.slice(-5, -3) // slice seconds from value
-    ms = val.slice(-3) // slice milliseconds from value
+    m = parseInt(val.slice(-7, -5)) || 0 // slice minutes from value
+    s = parseInt(val.slice(-5, -3)) || 0 // slice seconds from value
+    ms = parseInt(val.slice(-3)) // slice milliseconds from value
     // set text for timeElapsed element to dragged to location
     $('#timeElapsed').text(formatTime(m, s, ms, 'string'))
     // set rangeVal for dragged to location
@@ -230,41 +271,47 @@ $( () => {
     $('#range')[0].value = rangeVal
     // set current location for further time manipulation and tracking purposes
     currentLocation = formatTime(m, s, ms, 'number')
-    console.log(currentLocation);
+    // console.log(currentLocation);
   }
 
 
   // check formatting of video length inputs for strings
   const formatTime = (m, s, ms, type) => {
-    // minutes (m) and seconds (s) should always have two placeholders
-    if(m.length < 1) {
-      m = '00'
-    }
-    if(m.length < 2) {
-      m = '0' + m
-    }
-    if(s.length < 1) {
-      s = '00'
-    }
-    if(s.length < 2) {
-      s = '0' + s
-    }
-    // milliseconds (ms) should always have 3 placeholders
-    if(ms.length < 1) {
-      ms = '000'
-    }
-    if(ms.length < 2) {
-      ms = '00' + ms
-    }
-    if(ms.length < 3) {
-      ms = '0' + ms
-    }
+    // format time units to string for formatting to mm:s.ms
+      let mSF = m.toString()
+      let sSF = s.toString()
+      let msSF = ms.toString()
+      // minutes (m) and seconds (s) should always have two placeholders
+      if(mSF.length < 1) {
+        mSF = '00'
+      }
+      if(mSF.length < 2) {
+        mSF = '0' + mSF
+      }
+      if(sSF.length < 1) {
+        sSF = '00'
+      }
+      if(sSF.length < 2) {
+        sSF = '0' + sSF
+      }
+      // milliseconds (ms) should always have 3 placeholders
+      if(msSF.length < 1) {
+        msSF = '000'
+      }
+      if(msSF.length < 2) {
+        msSF = '00' + msSF
+      }
+      if(msSF.length < 3) {
+        msSF = '0' + msSF
+      }
+
+
     if(type === 'string') {
-      timeString = `${m}:${s}.${ms}` // set timeString variable
+      timeString = `${mSF}:${sSF}.${msSF}` // set timeString variable
       return timeString // return a string object
     }
     if (type === 'number') {
-      numString = parseInt(`${m}${s}${ms}`)
+      numString = parseInt(`${mSF}${sSF}${msSF}`)
       return numString // return a number object
     }
 
@@ -278,14 +325,17 @@ $( () => {
   $('form').on('submit', (e) => {
     // prevent default form submission action
     e.preventDefault()
-    let m = $('#vidMM')[0].value // variable holds minutes input
-    let s = $('#vidSS')[0].value // variable holds seconds input
-    let ms = $('#vidMS')[0].value // variable holds milliseconds input
+
+    let m = parseInt($('#vidMM')[0].value) || 0 // variable holds minutes input
+    let s = parseInt($('#vidSS')[0].value) || 0 // variable holds seconds input
+    let ms = parseInt($('#vidMS')[0].value) || 0 // variable holds milliseconds input
+
     // set video length as number and set max value for range element
-    vidTimeNumber = formatTime(m, s, s, 'number')
+    vidTimeNumber = formatTime(m, s, ms, 'number')
+    console.log(vidTimeNumber);
     $('#range').attr('max', vidTimeNumber)
     // set video length as string and set value for vidLength element
-    vidTimeString = formatTime(m, s, s, 'string')
+    vidTimeString = formatTime(m, s, ms, 'string')
     $('#vidLength').text(vidTimeString)
   })
 
@@ -306,6 +356,7 @@ $( () => {
         play = true
         // console.log(vf)
       } else if(play === true) { // functionality to stop timer
+        console.log(currentLocation);
         // stop timer interval
         clearInterval(handleTimer)
         // change text on stopStart button
@@ -313,7 +364,7 @@ $( () => {
         // set play to false for toggling of button functionality
         play = false
         // find object at current vf index and set endTime key value
-        vf[vfIndex].endTime = currentLocation
+        // vf[vfIndex].endTime = currentLocation
         // increment vfIndex for next pair
         vfIndex ++
         // console.log(vf)
@@ -331,7 +382,7 @@ $( () => {
     s = 0
     ms = 0
     $('#range')[0].value = 0
-    $('#timeElapsed').text(formatTime(m.toString(), s.toString(), ms.toString(), 'string'))
+    $('#timeElapsed').text(formatTime(m, s, ms, 'string'))
   })
 
 
